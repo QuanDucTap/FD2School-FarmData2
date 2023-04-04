ea3dcb077fd3a67853caa17911a1b1e5f5e4110e
phong260702 <phongdangquoc123@gmail.com>
2023-04-03 23:19:00 -0400

    Adding test for seeding field and trays field

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.tray.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.tray.defaults.spec.js
@@ -6,6 +6,7 @@ describe('Test the tray seeding ', () => {
 
     })
 
+
     it("Check the tray seeding section when 'Tray' is selected ", () => {
 cy.get ("[data-cy=tray-seedings]").check()
 cy.get("[data-cy=tray-selection]").should("be.visible")
@@ -16,13 +17,26 @@ cy.get("[data-cy=tray-selection]").should("be.visible")
         cy.get ("[data-cy=tray-seedings]").check()
         
         cy.get("[data-cy=tray-area-selection]>[data-cy=dropdown-input]").should("be.visible")
-    
 
 })
 
+it("Check tray field", () => {
+    cy.get("[data-cy=num-tray-input]").should("not.visible");
+    cy.get("[data-cy=tray-seedings]").click();
+    cy.get("[data-cy=num-tray-input]").should("be.visible")
+                                    .should("not.be.disabled")
+                                    .children().should("be.empty")
+                                    .type(100).should("have.value", 100);
+});
 
-
-
+it("Check seed field", () => {
+    cy.get("[data-cy=num-seed-input]").should("not.be.visible");
+    cy.get("[data-cy=tray-seedings]").click();
+    cy.get("[data-cy=num-seed-input").should("be.visible")
+                                    .should("not.be.disabled")
+                                    .children().should("be.empty")
+                                    .type(110).should("have.value", 110);
+});
 
     
 })
