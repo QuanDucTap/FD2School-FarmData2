89d9e7b50bc8f57f1bd2e636018a4b3a34f16e91
qnhn22 <nhatquan234@gmail.com>
2023-04-05 11:42:00 -0400

    Write tests for area dropdown and cells/tray field in Tray Seeding Input

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.tray.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.tray.defaults.spec.js
@@ -20,6 +20,31 @@ cy.get("[data-cy=tray-selection]").should("be.visible")
 
 })
 
+it("Test area dropdown", () => {
+    cy.get("[data-cy=tray-seedings]").check()
+    cy.get("[data-cy=tray-area-selection] > [data-cy=dropdown-input]").children()
+    .should("have.length", 5)
+    cy.get("[data-cy=tray-area-selection] > [data-cy=dropdown-input] > [data-cy=option0")
+    .should("have.value", "CHUAU")
+    cy.get("[data-cy=tray-area-selection] > [data-cy=dropdown-input] > [data-cy=option2")
+    .should("have.value", "JASMINE")
+    cy.get("[data-cy=tray-area-selection] > [data-cy=dropdown-input] > [data-cy=option4")
+    .should("have.value", "SEEDING BENCH")
+})
+
+it("Test cells/tray field", () => {
+    cy.get("[data-cy=tray-seedings]").check()
+    cy.get("[data-cy=num-cell-input]")
+    .should("be.visible")
+    cy.get("[data-cy=num-cell-input] > [data-cy=text-input]")
+    .should("not.be.disabled")
+    .should("be.empty")
+    cy.get("[data-cy=num-cell-input] > [data-cy=text-input]")
+    .type("22").blur()
+    cy.get("[data-cy=num-cell-input] > [data-cy=text-input]")
+    .should("have.value", "22")
+})
+
 it("Check tray field", () => {
     cy.get("[data-cy=num-tray-input]").should("not.visible");
     cy.get("[data-cy=tray-seedings]").click();
